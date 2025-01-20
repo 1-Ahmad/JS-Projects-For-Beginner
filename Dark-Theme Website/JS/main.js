2ebe9587522f13c87ff6a7f7ad814e2564809da0
Ahmad Sweid <152951566+1-Ahmad@users.noreply.github.com>
2025-01-20 14:08:39 -0800

    Add files via upload

---
+++ Dark-Theme Website/JS/main.js
@@ -0,0 +1,10 @@
+let icon= document.querySelector("#icon") ;
+icon.addEventListener("click", ()=>{
+  document.body.classList.toggle("dark-theme")
+  if( document.body.classList.contains("dark-theme")){
+    icon.src= "./imgs/sun.png" ;
+  }
+  else{
+    icon.src= "./imgs/moon.png" ;
+  }
+}) ;
