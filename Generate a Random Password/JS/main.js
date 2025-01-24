159db0e9a401be788eea507930317086d59700b9
Ahmad Sweid <152951566+1-Ahmad@users.noreply.github.com>
2025-01-24 06:05:46 -0800

    Add files via upload

---
+++ Generate a Random Password/JS/main.js
@@ -0,0 +1,23 @@
+let passwordBox = document.querySelector("#password");
+let length = 12;
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let symbol = "@#$%^&*()_+~|}{][></-=";
+let allChars = upperCase + lowerCase + numbers + symbol;
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copyPassword(){
+  passwordBox.select() ;
+  document.execCommand("copy") ;
+}
